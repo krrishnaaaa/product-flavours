557b06197df3a7b97f08d58d05e8448c475d7240
krrishnaaaa <krishna@pcsalt.com>
2020-11-15 23:09:53 +0530

    removed unused code

--- app/src/main/java/com/pcsalt/example/productflavors/MainActivity.java
+++ app/src/main/java/com/pcsalt/example/productflavors/MainActivity.java
@@ -1,7 +1,5 @@
 package com.pcsalt.example.productflavors;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,7 +9,6 @@ import com.pcsalt.example.productflavors.utils.MyLog;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
     private static final String TAG = "MainActivity";
 
     @Override
@@ -19,17 +16,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.text_view);
+        TextView textView = (TextView) findViewById(R.id.text_view);
 
-        String version = "";
-        try {
-            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-            version = pInfo.versionName;
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        textView.setText(getString(R.string.app_name) + " - " + version);
+        textView.setText(getString(R.string.app_name) + " - " + BuildConfig.VERSION_NAME);
 
         MyLog.e(TAG, "some error log");
     }
